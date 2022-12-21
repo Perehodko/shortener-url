b45f74b9c748049dd93c2b325172928240f8ae3b
nperekhodko <nperekhodko@ozon.ru>
2022-12-21 14:59:11 +0300

    new with gorilla mux

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,64 +1,97 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var storageURLs = make(map[string]string)
 
-func getURLAndCut(w http.ResponseWriter, r *http.Request) {
+func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusTemporaryRedirect)
-
-		url := r.URL.RequestURI()
-		w.Write([]byte(r.Host + storageURLs[url]))
-		//w.Write([]byte(`lflflflf`))
-
-		w.Header().Set("Location", r.Host+storageURLs[url])
-
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		urlForCuts := r.URL.RequestURI()
-
-		if len(urlForCuts[1:]) >= 3 {
-			cutURL := urlForCuts[0:3]
-			storageURLs[cutURL] = urlForCuts
-			w.Write([]byte(cutURL))
-
-		} else {
-			storageURLs[urlForCuts] = urlForCuts
-			w.Write([]byte(urlForCuts))
-		}
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "not found"}`))
-	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "get called"}`))
 }
 
-func returnLongURL(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		url := r.URL.RequestURI()
-		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte(storageURLs[url]))
-	default:
-		w.Write([]byte(r.Method))
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// читаем Body
+	bodeData, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
 	}
+	//фигачим в json
+	var dat map[string]interface{}
+	if err := json.Unmarshal(bodeData, &dat); err != nil {
+		panic(err)
+	}
+	//fmt.Println(dat["URL"])
+
+	urlForCuts := dat["URL"].(string)
+	getHost := r.Host
+
+	//resForCut := strings.ReplaceAll(urlForCuts, "http://"+getHost+"/", "")
+	//w.Write([]byte(resForCut))
+	shotrLink := shorting()
+	fmt.Println(shotrLink)
+	shortURL := "http://" + getHost + "/" + shotrLink
+	fmt.Println(shortURL)
+
+	storageURLs[shotrLink] = urlForCuts
+	//fmt.Println(storageURLs)
+
+	w.Write([]byte(shortURL))
+
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message": "not found"}`))
+}
+
+func redirectTo(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	//var link string
+	vars := mux.Vars(r)
+	shortURl := vars["id"]
+	initialURL := storageURLs[shortURl]
+
+	//fmt.Println(storageURLs)
+	//fmt.Println("initialURL:", initialURL)
+
+	http.Redirect(w, r, initialURL, http.StatusSeeOther)
+	w.Header().Set("Location", initialURL)
+
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func shorting() string {
+	b := make([]byte, 5)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
 
 func main() {
-	// маршрутизация запросов обработчику
-	http.HandleFunc("/", getURLAndCut)
-	//http.HandleFunc("/", returnLongURL)
+	r := mux.NewRouter()
+	//r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", notFound)
 
-	// конструируем свой сервер
-	server := &http.Server{
-		Addr: "localhost:8080",
-	}
+	r.HandleFunc("/{id}", redirectTo).Methods(http.MethodGet)
 
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Perehodko/shortener-url
 
 go 1.18
+
+require github.com/go-redis/redis v6.15.9+incompatible
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.24.1 // indirect
+	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
