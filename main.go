f95278a6de60ded4651cf495fdc03b50567751e1
nperekhodko <nperekhodko@ozon.ru>
2022-12-28 12:11:16 +0500

    add main in oot dir with main logic

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/Perehodko/shortener-url/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(3 * time.Second))
+
+	r.Post("/", handlers.GetURLForCut)
+	r.Get("/{id}", handlers.RedirectTo)
+	r.Get("/", handlers.NotFoundFunc)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
