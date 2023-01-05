6bf2a99916ef5faeb5410761a8f0394d47693c7e
nperekhodko <nperekhodko@ozon.ru>
2023-01-05 19:30:54 +0500

    add storage package

---
+++ internal/storage/storage.go
@@ -0,0 +1,29 @@
+package storage
+
+type NewInterface interface {
+	PutURLInStorage(shortLink, urlForCuts string)
+	GetURLFromStorage(shortURL string) string
+}
+
+type URLStorage struct {
+	URLs map[string]string
+}
+
+// NewURLStore returns a new/empty URLStorage
+func NewURLStore() *URLStorage {
+	return &URLStorage{
+		URLs: make(map[string]string),
+	}
+}
+
+func (s *URLStorage) PutURLInStorage(shortLink, urlForCuts string) {
+	s.URLs[shortLink] = urlForCuts
+}
+
+func (s *URLStorage) GetURLFromStorage(shortURL string) string {
+	initialURL := s.URLs[shortURL]
+	if initialURL == "" {
+		return ""
+	}
+	return initialURL
+}
